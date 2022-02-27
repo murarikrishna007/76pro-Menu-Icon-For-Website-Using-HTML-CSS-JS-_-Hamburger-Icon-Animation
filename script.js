d9ad9d08e57f3e5eba9b131802782a768393b5da
murarikrishna007 <murarikrishna0071994@gmail.com>
2022-02-27 12:17:58 +0530

    hamburger icon using html,css,js

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("krishna");
+
+let menu = document.getElementById("menu");
+let menu2 = document.getElementById("menu2");
+
+menu.onclick = function(){
+    menu.classList.toggle("openmenu");
+
+}
+
+menu2.onclick = function(){
+    menu2.classList.toggle("openmenu2");
+
+}
